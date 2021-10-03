92ea1b000ba29e9f7fad51ea4e19f3081bbcab3c
Taha Abdoh <taha.abdoh@gmail.com>
2021-10-03 13:24:23 +0200

    watching using child_process

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const PACKAGE_INFO = require("./package.json");
 const THEME_PATH = process.cwd();
-const SATARTER_THEME_URL = "https://github.com/tahaabdoh/demo-template";
+const SATARTER_THEME_URL = "http://github.com/SallaApp/theme-y";
 const LOGIN_URL = "https://dashboard-26c3cd35add3468fc189c714bd1a1345.salla.group/themes/new"
 
 module.exports = {
