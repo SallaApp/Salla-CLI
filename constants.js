d3e5b4ab980bd190e89780c1b4d90b6290958fb9
Taha Abdoh <taha.abdoh@gmail.com>
2021-10-06 15:02:38 +0200

    read token from config.json

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ const PACKAGE_INFO = require(process.cwd() + "/package.json");
 const NODE_ENGINES = "^10 || ^12 || ^14";
 
 const SATARTER_THEME_URL = "http://github.com/SallaApp/theme-y";
-const LOGIN_URL = "https://dashboard-26c3cd35add3468fc189c714bd1a1345.salla.group/themes/new"
+const LOGIN_URL = "https://dashboard-26c3cd35add3468fc189c714bd1a1345.salla.group/accounts/oauth/cli"
 
 module.exports = {
   PACKAGE_INFO,
