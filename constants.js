06475d975acdb5daa6cd471cbac75759cd9c260a
Taha Abdoh <taha.abdoh@gmail.com>
2021-10-11 18:12:14 +0200

    add repository url to theme.config

--- constants.js
+++ constants.js
@@ -7,11 +7,14 @@ const SATARTER_THEME_INFO = {
   repo: "jamalY"
 };
 
+
 const LOGIN_URL = "https://dashboard-26c3cd35add3468fc189c714bd1a1345.salla.group/accounts/oauth/cli"
+const DRAFT_THEME_ENDPOINT = "https://dashboard-26c3cd35add3468fc189c714bd1a1345.salla.group/admin/v2/theme"
 
 module.exports = {
   THEME_PATH,
   SATARTER_THEME_INFO,
   LOGIN_URL,
-  NODE_ENGINES
+  NODE_ENGINES,
+  DRAFT_THEME_ENDPOINT
 };
