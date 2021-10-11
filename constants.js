c6ab2489949617820f7c500bee0c8da63252e968
Taha Abdoh <taha.abdoh@gmail.com>
2021-10-11 12:08:19 +0200

    clone the latest release

--- constants.js
+++ constants.js
@@ -1,12 +1,17 @@
 const THEME_PATH = process.cwd();
 const NODE_ENGINES = "^10 || ^12 || ^14";
 
-const SATARTER_THEME_URL = "https://github.com/tahaabdoh/jamalY";
+const SATARTER_THEME_INFO = {
+  url: "https://github.com/tahaabdoh/jamalY",
+  owner: "tahaabdoh",
+  repo: "jamalY"
+};
+
 const LOGIN_URL = "https://dashboard-26c3cd35add3468fc189c714bd1a1345.salla.group/accounts/oauth/cli"
 
 module.exports = {
   THEME_PATH,
-  SATARTER_THEME_URL,
+  SATARTER_THEME_INFO,
   LOGIN_URL,
   NODE_ENGINES
 };
