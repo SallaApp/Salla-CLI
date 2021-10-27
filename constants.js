733c1290e5152d442fd1650042f8d6001c463bcc
Jamal Al-Matari <jalmatari@gmail.com>
2021-10-27 17:16:07 +0300

    🐞 Show `salla theme serve` Errors (#6)

--- constants.js
+++ constants.js
@@ -1,6 +1,7 @@
 global.BASE_PATH = process.cwd();
 global.NODE_ENGINES = "^10 || ^12 || ^14";
-global.BASE_URL = 'https://dashboard-26c3cd35add3468fc189c714bd1a1345.salla.group';//"https://s.salla.test";//"https://dashboard-26c3cd35add3468fc189c714bd1a1345.salla.group";
+global.BASE_URL = 'https://dashboard-26c3cd35add3468fc189c714bd1a1345.salla.group';
+//global.BASE_URL ="https://s.salla.test";
 
 global.BASE_THEME = {
     url : "https://github.com/SallaApp/theme-y",
