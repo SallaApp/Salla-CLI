ea8ad003f4a1bfdf4de35410b87515dd14141687
Taha Abdoh <taha.abdoh@gmail.com>
2021-11-04 11:45:02 +0100

    added salla cli text to help

--- bin/index.js
+++ bin/index.js
@@ -9,6 +9,15 @@ const commander = require("commander");
 const program = new commander.Command();
 program.version('0.0.2');
 
+program.addHelpText('before', `
+  _____       _ _          _____ _      _____ 
+ / ____|     | | |        / ____| |    |_   _|
+| (___   __ _| | | __ _  | |    | |      | |  
+ \\___ \\ / _\` | | |/ _\` | | |    | |      | |  
+ ____) | (_| | | | (_| | | |____| |____ _| |_ 
+|_____/ \\__,_|_|_|\\__,_|  \\_____|______|_____|
+`.green);
+
 const themeCommands = require("./theme");
 const appCommands = require("./app");
 const devCommands = require("./dev");
