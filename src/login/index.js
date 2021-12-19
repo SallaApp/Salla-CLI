d6f9aee426c739538473acdfc3106b85087d36b7
Salah Alkhwlani <yemenifree@yandex.com>
2021-12-19 16:19:40 +0300

    Update index.js

--- src/login/index.js
+++ src/login/index.js
@@ -14,18 +14,35 @@ const request = require("request");
 module.exports = async function (options) {
   // WIP
   let salla_endpoint = "https://salla.partners/auth/cli";
-  let salla_ws_endpoint = "ws://salla.partners/auth/cli";
+  let salla_ws_endpoint = "ws://ws.salla.group/connection/websocket"; // ws.salla.cloud
 
   const tokens = await AuthManager.isSallaTokenValid();
 
-  let rand_idn = generateRandome(64);
-  let testEndpoint = `${salla_endpoint}?identify=${rand_idn}`;
+  let randromIdentify = generateRandome(64);
+  let generateConnectionTokenEndpoint = `${salla_endpoint}?identify=${randromIdentify}`;
 
-  request(testEndpoint, function (error, response, body) {
+  request(generateConnectionTokenEndpoint, function (error, response, body) {
     console.error("error:", error); // Print the error if one occurred
     console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
     console.log("body:", body); // Print the HTML for the Google homepage.
   });
+  
+  Salla.websocket
+    .init({
+      connectionEndpoint: salla_ws_endpoint,
+      connectionToken: 'the connection token'
+    })
+    .connect()
+    .subscribe("salla:cli#"+randromIdentify, function(event){
+        const data = event.data;
+        if (!data.event) {
+          return;
+        }
+        const eventName = data.event.split('\\').slice(-1).pop();
+        // store the token in the auth file
+    });
+    
+  
   const ws = new WebSocket(salla_ws_endpoint);
 
   ws.on("open", function open() {
