21bbaa10e195dcf887010997834e63fc0455f220
Abdullah Altahery <kernelcod3@gmail.com>
2021-12-28 17:45:26 +0300

    fixes

--- dist/run.js
+++ dist/run.js
@@ -1,6 +1,6 @@
-var shell = require("shelljs");
-var opsys = process.platform;
-
+const shell = require("shelljs");
+let opsys = process.platform;
+const path = require("path");
 if (opsys === "darwin") {
   opsys = "macos";
 } else if (opsys === "win32" || opsys === "win64") {
@@ -9,4 +9,4 @@ if (opsys === "darwin") {
   opsys = "linux";
 }
 var myArgs = process.argv.slice(2);
-shell.exec("salla-" + opsys + " " + myArgs.join(" "));
+shell.exec(path.resolve("./salla-" + opsys + " " + myArgs.join(" ")));
